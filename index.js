ef6327ce19fe8c337783815924c0f656e33a9090
48vineet <70338vineet@gmail.com>
2025-01-18 23:48:35 +0530

    added and updated

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 const app = express();
 const port = 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 
 main()
   .then(() => {
@@ -23,6 +28,54 @@ async function main() {
 app.get("/", (req, res) => {
   res.send("Working");
 });
+
+// Index Route
+
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", { chats });
+})
+
+//New Route
+app.get("/chats/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+//Create route
+
+app.post("/chats", (req, res) => {
+  let { from, to, msg } = req.body;
+  let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date()
+  });
+  newChat.save().then((res) => {
+    console.log("chat was saved");
+  })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.redirect("/chats");
+});
+
+//Edit Route 
+app.get("/chats/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", { chat });
+});
+
+//Update Route
+app.put("/chats/:id", async (req, res) => {
+  let { id } = req.params;
+  let { msg: newMsg } = req.body;
+  let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg }, { runValidators: true, new: true });
+  // console.log(updatedChat);
+  res.redirect("/chats");
+})
 
 app.listen(port, () => console.log(`app listening on port ${3000}!`));
 
