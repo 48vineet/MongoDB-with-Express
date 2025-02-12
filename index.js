c45e14aa725b93c6e3c71ae3e439cdeadb3fe205
Vineet Mali <7038vineet@gmail.com>
2025-02-12 14:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
- 
+  
   
 main()    
   .then(() => {
