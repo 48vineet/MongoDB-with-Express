8a2b9003f46d26aaf63cc37a7db4be487154dcb5
Vineet Mali <7038vineet@gmail.com>
2025-02-19 23:09:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
   
-   
+    
 main()    
   .then(() => {
     console.log("connection Successful");
